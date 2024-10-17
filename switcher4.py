4ae10ff22aa3b8c7dc5ae208fe3e08b09f870ea4
Yaowen Mei <ywmei@google.com>
2024-10-17 19:39:50 +0000

    Reduce system call for ds-docs-switched
    Currently, switcher4 call `sed` multiply times for each file, and sed will perform steam editing (a lot of read/write calls line by line). This cause performance regression.
    
    m ds-docs-switched takes 1m20s to generate the out/soong/.intermediates/frameworks/base/api/ds-docs-switched/android_common/gen/ds-docs-switched.zip file.
    
    This get worse if the output directory is a fuse mounted dir, fuse will take 20 mins to finish this step even with kernel level passthrough.
    
    This fix reduce the run time by editing each of the file in RAM, and write back to disk once.
    With this fix, the generation of ds-docs-switched.zip reduced to 14s (without fuse), and 45s (with fuse).
    
    See trace file:
    Before this change no fuse: https://ui.perfetto.dev/#!/?s=a1d187ae767f8a17473812c5f644fc3e3cf4a4df
    Before this change with fuse:https://ui.perfetto.dev/#!/?s=ed77cc570ae3c12d7495c13ec1658a656acaa520
    After this change no fuse: https://ui.perfetto.dev/#!/?s=2edcb71f2bcb3cad4c8eb908c8bd148f5a88034c
    After this change with fuse: https://ui.perfetto.dev/#!/?s=47ec8b2ea092197f2a4d289cf08c45cbc2ab8dde
    
    Change-Id: I1e789e6e0780fb067c3cc0acb60773da510bf191
    Tested: sha256sum of the ds-docs-switched.zip file, before and after this change are the same.

--- switcher4.py
+++ switcher4.py
@@ -88,31 +88,46 @@ def insert_stub(doc, java, both):
     print("File: ", stubs, fn[1], end="\r")
 
   if (java):
-      java_stubs = java_stubs + 1
+    java_stubs = java_stubs + 1
   else:
-      kotlin_stubs = kotlin_stubs + 1
-
+    kotlin_stubs = kotlin_stubs + 1
 
   if (work):
-    if (java):
-      file_path = doc[len(java_ref_root)+1:]
-      stub = doc.replace(java_source_abs_path, kotlin_source_abs_path)
-      if (both):
-        slug1 = "sed -i 's/<\/h1>/{}/' {}".format("<\/h1>\\n{% setvar page_path %}_page_path_{% endsetvar %}\\n{% setvar can_switch %}1{% endsetvar %}\\n{% include \"reference\/_java_switcher2.md\" %}",doc)
-      else:
-        slug1 = "sed -i 's/<\/h1>/{}/' {}".format("<\/h1>\\n{% include \"reference\/_java_switcher2.md\" %}",doc)
-    else:
-      file_path = doc[len(kotlin_ref_root)+1:]
-      stub = doc.replace(kotlin_source_abs_path, java_source_abs_path)
-      if (both):
-        slug1 = "sed -i 's/<\/h1>/{}/' {}".format("<\/h1>\\n{% setvar page_path %}_page_path_{% endsetvar %}\\n{% setvar can_switch %}1{% endsetvar %}\\n{% include \"reference\/_kotlin_switcher2.md\" %}",doc)
-      else:
-        slug1 = "sed -i 's/<\/h1>/{}/' {}".format("<\/h1>\\n{% include \"reference\/_kotlin_switcher2.md\" %}",doc)
+    with open(doc, "r") as f:
+      file_content = f.read()
 
-    os.system(slug1)
-    if (both):
-      page_path_slug = "sed -i 's/_page_path_/{}/' {}".format(file_path.replace("/","\/"),doc)
-      os.system(page_path_slug)
+    if (java):
+      file_path = doc[len(java_ref_root) + 1 :]
+      if (both):
+        file_content = file_content.replace(
+            "</h1>",
+            "</h1>\n{% setvar page_path %}_page_path_{% endsetvar %}\n{% setvar"
+            " can_switch %}1{% endsetvar %}\n{% include"
+            ' "reference/_java_switcher2.md" %}',
+        )
+        file_content = file_content.replace("_page_path_", file_path)
+      else:
+        file_content = file_content.replace(
+            "</h1>", '</h1>\n{% include "reference/_java_switcher2.md" %}'
+        )
+    else:
+      file_path = doc[len(kotlin_ref_root) + 1 :]
+      if (both):
+        file_content = file_content.replace(
+            "</h1>",
+            "</h1>\n{% setvar page_path %}_page_path_{% endsetvar %}\n{% setvar"
+            " can_switch %}1{% endsetvar %}\n{% include"
+            ' "reference/_kotlin_switcher2.md" %}',
+        )
+        file_content = file_content.replace("_page_path_", file_path)
+      else:
+        file_content = file_content.replace(
+            "</h1>", '</h1>\n{% include "reference/_kotlin_switcher2.md" %}'
+        )
+
+    with open(doc, "w") as f:
+      f.write(file_content)
+    os.chmod(doc, 0o644)
 
 
 def scan_files(stem):
